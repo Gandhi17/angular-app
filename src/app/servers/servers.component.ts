af0e931fc035b355f9aa8d5aaa1d54c5cd02a15a
Gandhimathi Sethuraman <vardhini7@gmail.com>
2022-09-07 12:31:43 -0500

    l 30 completed

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '.app-servers',
-  templateUrl: './servers.component.html',
+  selector: 'app-servers',
+  //template: `
+ // <app-server></app-server>
+  //<app-server></app-server>`,
+  templateUrl:'./servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServer = false;
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000);
+   }
 
   ngOnInit(): void {
   }
---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-warning-alert',
+    template:`
+    <p>This is a warning alert!</p>
+    `
+})
+
+export class WarningAlertComponent{
+
+}
